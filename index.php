50a2d50060cd2cd8bfbae8b7b0b3a123b3798977
Yaroslav Shapoval <shapoval.yaroslav@gmail.com>
2014-04-17 15:45:59 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <title>MongoDB</title>
+</head>
+<body>
+  <?php echo 'Hello, world!'; ?>
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+</body>
+</html>
