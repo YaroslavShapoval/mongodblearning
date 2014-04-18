89ad91793a634c16dd8c1e1f4f4168ed2dc230ba
Yaroslav Shapoval <shapoval.yaroslav@gmail.com>
2014-04-18 17:46:09 +0300

    create add-by-click form

--- index.php
+++ index.php
@@ -1,16 +1,32 @@
 <!doctype html>
-<html lang="ru">
+<html lang='ru'>
 <head>
-  <meta charset="UTF-8">
+  <meta charset='UTF-8'>
   <title>MongoDB Learning</title>
+  <script src='//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js'></script>
+  <link rel='stylesheet' href='css/style.css'>
 </head>
 <body>
+
+<div class='wrapper'>
+
+<form action='' method='post'>
+  <div class='fields' id='dinamicForm'>
+    <!-- Here will be field rows -->
+  </div>
+
+  <div class='submit-buttons'>
+    <input type='submit' class='submit button' value='Отправить' />
+  </div>
+</form>
+
+</div>
 
   <?php echo '<p>Hello, world!</p>'; ?>
 
   <?php
     //echo phpinfo();
-  try {
+/*  try {
    // open connection to MongoDB server
    $conn = new Mongo('localhost');
 
@@ -40,11 +56,11 @@
    die('Error connecting to MongoDB server');
   } catch (MongoException $e) {
    die('Error: ' . $e->getMessage());
-  }
+  }*/
   ?>
 
   <?php echo '<p>Bye-bye!</p>'; ?>
 
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+  <script src='js/fields.js'></script>
 </body>
 </html>
---
+++ js/fields.js
@@ -0,0 +1,61 @@
+$(document).ready(function() {
+  var startingCount = 2;        // Изначально есть 2 строки полей
+  var currentCount = 0;         // Количество строк в данный момент
+  var root = $("#dinamicForm"); // Родительский элемент для добавления
+
+  // Вставляемый элемент
+  var $node = " \
+    <div class='fieldrow'> \
+      <div class='field'> \
+        <input type='text' class='fieldname' name='fieldname' placeholder='Field name' /> \
+      </div> \
+      <div class='field'> \
+        <input type='text' class='fieldvalue' name='fieldvalue' placeholder='Field value' /> \
+      </div> \
+      <div class='field'> \
+        <button type='button' class='add button'>+</button> \
+        <button type='button' class='remove button'>-</button> \
+      </div> \
+    </div> \
+  ";
+
+  // Функция для отображения кнопок "добавить"-"удалить"
+  // Кнопка "добавить" есть только в последней строке
+  // Кнопка "удалить" есть в каждой строке, кроме последней
+  function displayButtons() {
+    $('.fieldrow .add').css('display', 'none');
+    $('.fieldrow:last .add').css('display', 'inline');
+
+    $('.fieldrow .remove').css('display', 'inline');
+    $('.fieldrow:last .remove').css('display', 'none');
+  }
+
+  // Инициализация полей
+  for(var i = 0; i < startingCount; i++) {
+    root.after($node);
+    currentCount++;
+  }
+  displayButtons();
+  console.log(currentCount);
+
+  // После нажатия кнопки с классом .remove.button
+  // находим ближайший элемент с классом .fieldrow
+  // и удаляем его
+  $(document).on('click', '.remove.button', function() {
+    $(this).closest('.fieldrow').remove();
+    currentCount--;
+    displayButtons();
+    console.log(currentCount);
+  });
+
+
+  // После нажатия кнопки с классом .add.button
+  // находим ближайший элемент с классом .fieldrow
+  // и вставялем после него переменную $node
+  $(document).on('click', '.add.button', function() {
+    $(this).closest('.fieldrow').after($node);
+    currentCount++;
+    displayButtons();
+    console.log(currentCount);
+  });
+});
