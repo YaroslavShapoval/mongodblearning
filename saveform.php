bbe80ae5bb1589be8f1d40d6184225a529ec2bf6
Yaroslav Shapoval <shapoval.yaroslav@gmail.com>
2014-04-18 18:50:32 +0300

    connection to mongo on heroku: first attempt

--- saveform.php
+++ saveform.php
@@ -10,51 +10,47 @@
 <div class='wrapper'>
 <a href="./">Back home</a>
 <?php
-echo 'hello!';
+  echo '<p>Hello!</p>';
   $field_name = $_POST['fieldname'];
   $field_value = $_POST['fieldvalue'];
 
-  foreach( $field_name as $key => $field_name_element ) {
-    echo '<p>' . $field_name_element . ': ' . $field_value[$key] . '</p>';
+  foreach ($field_name as $key => $field_name_element) {
+    $field_value_element = $field_value[$key];
+    echo '<p>' . $field_name_element . ': ' . $field_value_element . '</p>';
+  }
+
+  try {
+    // Подкюлаемся к серверу MongoDB
+    //$hostname = 'localhost';
+    $hostname = getenv("MONGOHQ_URL");
+    $mongo = new Mongo($hostname);
+
+    // Выбираем БД
+    $dbname = 'dinamicForm';
+    $db = $mongo->selectDB($dbname);
+
+    // Выбираем коллекцию
+    $collection = $db->fields;
+
+    // Подготавливаем документ, соединяя поля
+    $item = array();
+    foreach ($field_name as $key => $field_name_element) {
+      $field_value_element = $field_value[$key];
+      $item["$field_name_element"] = "$field_value_element";
+    }
+
+    // Добавляем новый документ
+    $collection->insert($item);
+    echo 'Inserted document with ID: ' . $item['_id'];
+
+    // Отключаемся от сервера
+    $mongo->close();
+  } catch (MongoConnectionException $e) {
+    die('Error connecting to MongoDB server');
+  } catch (MongoException $e) {
+    die('Error: ' . $e->getMessage());
   }
 ?>
-
-  <?php
-    //echo phpinfo();
-/*
-  try {
-   // open connection to MongoDB server
-   $conn = new Mongo('localhost');
-
-   // access database
-   $db = $conn->test;
-
-   // access collection
-   $collection = $db->items;
-
-   // execute query
-   // retrieve all documents
-   $cursor = $collection->find();
-
-   // iterate through the result set
-   // print each document
-   echo $cursor->count() . ' document(s) found. <br/>';
-   foreach ($cursor as $obj) {
-    echo 'Name: ' . $obj['name'] . '<br/>';
-    echo 'Quantity: ' . $obj['quantity'] . '<br/>';
-    echo 'Price: ' . $obj['price'] . '<br/>';
-    echo '<br/>';
-   }
-
-   // disconnect from server
-   $conn->close();
-  } catch (MongoConnectionException $e) {
-   die('Error connecting to MongoDB server');
-  } catch (MongoException $e) {
-   die('Error: ' . $e->getMessage());
-  }
-*/
-  ?>
 
 </div>
 </body>
