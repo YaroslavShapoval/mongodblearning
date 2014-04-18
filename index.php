7b071f7b3a24bc0b787299702be87a5fa55b269f
Yaroslav Shapoval <shapoval.yaroslav@gmail.com>
2014-04-18 08:56:51 +0300

    mod index.php

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
   <title>MongoDB</title>
 </head>
 <body>
+
+  git clone git@heroku.com:mongolearning.git -o heroku
+
   <?php echo 'Hello, world!'; ?>
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 </body>
