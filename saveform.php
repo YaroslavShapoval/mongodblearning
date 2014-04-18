2fb9f3edb0b37f51ef15a5d259cff7d7497f3a6d
Yaroslav Shapoval <shapoval.yaroslav@gmail.com>
2014-04-18 20:19:34 +0300

    connection to mongo on heroku: attempt#7

--- saveform.php
+++ saveform.php
@@ -24,8 +24,9 @@
     //$hostname = 'localhost';
     echo "<p>i'm inside try/catch</p>";
     //$MONGOHQ_URL = "mongodb://heroku:m5Hr0c7KdcvAkDjkRv_MqbTKKXe2IfH966RR8EJ9nMWhalKHoSKwcnHuIJ3tdZ3xEkhQwT-2DExMPdTTyaJZQQ@lennon.mongohq.com:10026/app24267309";
-    $hostname = getenv("MONGOHQ_URL") ?: die('Missing MONGOHQ_URL environment variable');
-    $mongo = new MongoClient($hostname);
+    $hostname = getenv('MONGOHQ_URL');
+    //$hostname = getenv('MONGOHQ_URL') ?: die('Missing MONGOHQ_URL environment variable');
+    //$mongo = new Mongo($hostname);
 
     echo "<p>connection established</p>";
 
