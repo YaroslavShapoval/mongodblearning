2d65d3fbe672c04abd221b082dbf19a9bf2d5e36
Yaroslav Shapoval <shapoval.yaroslav@gmail.com>
2014-04-18 22:34:16 +0300

    wtf

--- index.php
+++ index.php
@@ -8,6 +8,30 @@
 </head>
 <body>
 
+<?php
+  # get the mongo db name out of the env
+  $mongo_url = parse_url(getenv("MONGOLAB_URI"));
+  $dbname = str_replace("/", "", $mongo_url["path"]);
+
+  # connect
+  $m   = new Mongo(getenv("MONGO_URL"));
+  $db  = $m->$dbname;
+  $col = $db->access;
+
+  # insert a document
+  $visit = array( "ip" => $_SERVER["HTTP_X_FORWARDED_FOR"] );
+  $col->insert($visit);
+
+  # print all existing documents
+  $data = $col->find();
+  foreach($data as $visit) {
+    echo "<li>" . $visit["ip"] . "</li>";
+  }
+
+  # disconnect
+  $m->close();
+?>
+
   <div class='wrapper'>
     <form action='saveform.php' method='post'>
       <div class='fields' id='dinamicForm'>
